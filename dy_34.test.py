5b3ee9d89a7f02f34a0deb64e387dc9ecd6d9cd6
Muhydeen Adelani <139554441+DataNaija@users.noreply.github.com>
2023-08-01 22:44:53 -0600

    Update dy_34.test.py

--- dy_34.test.py
+++ dy_34.test.py
@@ -10,13 +10,13 @@ if path == "R":
 elif path == "L":
     current_wave = input("Enter the water direction? S, W \n")
     if current_wave == "S":
-            print(f'Sorry you choice is  {current_wave}, GAME OVER')
+            print(f'Sorry you choice is  {current_wave}, GAME OVER!!!')
     elif current_wave == "W":
             color_type = input("Enter the color type you want to choose from: R, B, Y \n")
             if  color_type == "B":
-                print(f'You choice is  {color_type}, GAME OVER')
+                print(f'You choice is  {color_type}, GAME OVER!!!')
             elif color_type == "R":
-                print(f'You choice is {color_type}, GAME OVER')
+                print(f'You choice is {color_type}, GAME OVER!!!')
             else:
                             
                 print(f'You choice is {color_type}, YOU WIN')
